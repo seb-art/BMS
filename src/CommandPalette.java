ed08c7cc1f0ae86bcd82299f3ffbc74db0442735
seb-art <kimanzikims300@gmail.com>
2023-03-18 12:27:48 +0300

    list of commands

--- src/CommandPalette.java
+++ src/CommandPalette.java
@@ -1,3 +1,74 @@
 public class CommandPalette {
-    
+    private String a = "add building";
+    private String d = "display building";
+    private String m = "modify building";
+    private String e = "edit rooms";
+    private String u = "undo";
+    private String r = "redo";
+    private String l = "list undo/redo";
+    private String x = "exit system";
+
+    public String getA() {
+        return a;
+    }
+
+    public void setA(String a) {
+        this.a = a;
+    }
+
+    public String getD() {
+        return d;
+    }
+
+    public void setD(String d) {
+        this.d = d;
+    }
+
+    public String getM() {
+        return m;
+    }
+
+    public void setM(String m) {
+        this.m = m;
+    }
+
+    public String getE() {
+        return e;
+    }
+
+    public void setE(String e) {
+        this.e = e;
+    }
+
+    public String getU() {
+        return u;
+    }
+
+    public void setU(String u) {
+        this.u = u;
+    }
+
+    public String getR() {
+        return r;
+    }
+
+    public void setR(String r) {
+        this.r = r;
+    }
+
+    public String getL() {
+        return l;
+    }
+
+    public void setL(String l) {
+        this.l = l;
+    }
+
+    public String getX() {
+        return x;
+    }
+
+    public void setX(String x) {
+        this.x = x;
+    }
 }
